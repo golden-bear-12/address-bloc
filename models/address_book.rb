acbf78a8c1914a89151da53a7e162766cc0cf6a9
golden-bear-12 <m.honrychs@gmail.com>
2016-03-01 22:06:18 -0500

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,7 +8,6 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-
     index = 0
     entries.each do |entry|
       if name < entry.name
@@ -17,5 +16,16 @@ class AddressBook
       index += 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |match|
+        if name == entry.name
+          break
+        end
+        index += 1
+      end
+    entries.delete_at(index)
   end
 end
